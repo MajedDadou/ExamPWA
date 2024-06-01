d568b4177b36e67144feb623b27c9af725c1b52f
MajedDadou <91125807+MajedDadou@users.noreply.github.com>
2024-06-01 19:27:08 +0200

    programme

--- latest/src/Component/Section.jsx
+++ latest/src/Component/Section.jsx
@@ -1,12 +1,23 @@
-// Section.jsx
+// src/components/Section.jsx
 import React from 'react';
 import '../Styles/Section.css';
 
-const Section = ({ id, title, children, customStyles }) => {
+const Section = ({ id, title, children, customStyles, content }) => {
   return (
     <div id={id} className="section" style={customStyles}>
       <h2>{title}</h2>
-      {children}
+      {content ? (
+        <ul className="content-list">
+          {content.map((item, index) => (
+            <li key={index}>
+              <span className="left">{item.left}</span>
+              <span className="right">{item.right}</span>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        children
+      )}
     </div>
   );
 };
--- latest/src/pages/Information.jsx
+++ latest/src/pages/Information.jsx
@@ -1,35 +1,75 @@
-// Information.jsx
+// src/pages/Information.jsx
 import React from 'react';
 import Section from '../Component/Section';
 import Tab from '../Component/tab';
 import '../Styles/Information.css';
+import Title from '../Component/Title';
 
 const Information = () => {
+    const openingHours = [
+        { left: 'alle dage fra 1.april - 31. oktober:', right: '10:00 - 17:00' },
+
+    ];
+    const annualCardPrices = [
+        { left: 'Voksen', right: '489 kr.' },
+        { left: 'Barn 3-13 år', right: '310 kr.' },
+    ];
+    const prices = [
+        { left: 'Voksne', right: '200 kr.' },
+        { left: 'Børn 3-13 år', right: '130 kr.' },
+        { left: 'Børn 0-1 år', right: 'Gratis' },
+        { left: 'Senior +65 år', right: '180 kr.' },
+
+    ];
+    const feedingHours = [
+        { left: 'Søbjørne', right: '10:30-11:00' },
+        { left: '', right: '15:30-16:00' },
+        { left: '', right: '' },
+
+        { left: 'Løver', right: '16:15-16:45' },
+        { left: 'Kænguruer', right: '11:30-12:00' },
+        { left: 'Zebra', right: '13:00-13:30' },
+
+    ];
+    const tourPrices = [
+        { left: 'Cykelbåde', right: '10 kr.' },
+        { left: 'Karrusel', right: '5 kr.' },
+        { left: 'Trækketur på pony', right: '25 kr.' },
+        { left: 'Kajak', right: '5 kr.' },
+
+    ];
+
     return (
         <div className="information-page">
+            <Title TitleText={'Information'} />
             <div className="tabs">
-                <Tab to="#opening-hours" label="Opening Hours" />
-                <Tab to="#prices" label="Prices" />
-                <Tab to="#annual-card" label="Annual Card" />
-                <Tab to="#feeding-hours" label="Feeding Hours" />
+                <Tab to="#opening-hours" label="Åbningstider" />
+                <Tab to="#prices" label="Billetpriser" />
+                <Tab to="#annual-card" label="Fordele med
+årskort!" />
+                <Tab to="#feeding-hours" label="Fodringstider" />
+                <Tab to="#tour" label="Turpriser" />
+
             </div>
 
-            <Section id="opening-hours" title="Opening Hours"customStyles={{ backgroundColor: '#2A444D' }}>
-                <p>Details about opening hours...</p>
-            </Section>
+            <Section id="opening-hours" title="Åbningstider" customStyles={{ backgroundColor: '#2A444D' }} content={openingHours} />
 
-            <Section id="prices" title="Prices" customStyles={{ backgroundColor: '#684024' }}>
-                <p>Details about prices...</p>
-            </Section>
+            <Section id="prices" title="Billetpriser" customStyles={{ backgroundColor: '#684024' }} content={prices} />
 
-            <Section id="annual-card" title="Annual Card" customStyles={{ backgroundColor: '#0C4425' }}>
-                <p>Details about the annual card...</p>
+            <Section id="annual-card" title="Fordele med årskort!" customStyles={{ backgroundColor: '#0C4425' }}>
+                <p>
+                    Med Årskort får I eksklusive rabatter i vores restauranter, caféer, kiosken samt souvenirshoppen. <br /> <br />
+                    Derudover giver årskort adgang til en unik oplevelse med dyrene, hvor både børn og voksne kan være med til at fordre dyrene. <br /><br />
+                    Børn (3-13 år) kan også give 50% på billetprisen til et andet barn i samme aldersgruppe. <br />
+                </p>
+                <Section content={annualCardPrices} customStyles={{margin: '0', padding: '0', boxShadow: 'none' }} />
                 <button className="buy-button">Buy Annual Card</button>
             </Section>
 
-            <Section id="feeding-hours" title="Feeding Hours" customStyles={{ backgroundColor: '#2A444D' }}>
-                <p>Details about feeding hours...</p>
+            <Section id="feeding-hours" title="Feeding Hours" customStyles={{ backgroundColor: '#2A444D' }} content={feedingHours}>
             </Section>
+            <Section id="tour" title="Turpriser" customStyles={{ backgroundColor: '#684024' }} content={tourPrices} />
+
         </div>
     );
 };
